7534d4aa0fac32f854131989d10cdce362394318
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-07 03:43:31 -0800

    Added functionality to webapp
    Added basic functionality to index.js and added container to index.html.
    Renamed the header img to make it shorter and more descriptive
    exported/imported data.js
    Started adding to index.css

---
+++ index.js
@@ -0,0 +1,35 @@
+import menuArray from "./data";
+
+function getMenuHtml(menuArr) {
+    return menuArr.map(item => {
+        const {
+            name,
+            ingredients,
+            id,
+            price,
+            emoji
+        } = item
+        return`
+            <section class="card">
+                <h1 class"emoji">${emoji}</h1>
+                <div id="${id}">
+                    <h2>${name}</h2>
+                    <p>${ingredients}</p>
+                    <h3>${price}</h3>
+                </div>
+                <button id="add-to-cart">+</button>
+            </section>`
+    }).join('')
+}
+
+function checkoutButton(display) {
+    if(display) {
+        return`
+            <section class="button">
+                <button type="button">Checkout</button>
+            
+            </section>`
+    }
+}
+
+document.getElementById('container').innerHTML = getMenuHtml(menuArray)
