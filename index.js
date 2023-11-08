fc73deee2297d5e2800bcbb8482343040e67ea27
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-08 02:53:51 -0800

    added checkout button display
    Checkout button now displays when the add-to-cart "+" button is pressed.
    Checkout button also now sets the Checkout modal box display property to block, so that it displays on the screen

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function getMenuHtml(menuArr) {
             price,
             emoji
         } = item
-        return`
+        return `
             <section class="card">
                 <h1 class"emoji">${emoji}</h1>
                 <div id="${id}">
@@ -30,26 +30,33 @@ document.getElementById('container').innerHTML = getMenuHtml(menuArray)
 const addToCartButtons = document.querySelectorAll('#add-to-cart')
 const checkoutContainer = document.getElementById('checkout-container')
 
-let buttonDisplay = false
+let checkoutButtonDisplay = false //Varible to initialize checkout buttons visibility
+let checkoutButtonCreated = false //Varible to check if the button exists
 
 addToCartButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    buttonDisplay = true;
-    if (buttonDisplay) {
-        const checkoutButtonExists = checkoutContainer.querySelector('#checkout')
-
-        if (!checkoutButtonExists) {
-            const checkoutButtonHTML = `
+    button.addEventListener('click', (e) => {
+        checkoutButtonDisplay = true
+        if (checkoutButtonDisplay) {
+            if (!checkoutButtonCreated) {
+                const checkoutButtonHTML = `
         <section class="button">
             <button type="button" id="checkout">Checkout</button>
         </section>`
-        const checkoutButtonElement = document.createElement('div')
-        checkoutButtonElement.innerHTML = checkoutButtonHTML
-        checkoutContainer.appendChild(checkoutButtonElement)
+                const checkoutButtonElement = document.createElement('div')
+                checkoutButtonElement.innerHTML = checkoutButtonHTML
+                checkoutContainer.appendChild(checkoutButtonElement)
+                checkoutButtonCreated = true
+            }
+            // Now that the checkout button exists, it can be used
+
+            const checkoutButton = document.getElementById('checkout')
+            checkoutButton.addEventListener('click', () => {
+                console.log('clicked')
+                displayCheckout(true)
+            })
         }
-    }
-    console.log('clicked:', e.target.parentElement.id);
-  });
+        console.log('clicked:', e.target.parentElement.id);
+    });
 });
 
 //Submit Order Modal Box
@@ -57,3 +64,9 @@ const submitModal = document.getElementById('order-submit-modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const orderForm = document.getElementById('order-form')
 const submitOrder = document.getElementById('modal-submit-btn')
+
+function displayCheckout(checkoutClick = false) {
+    console.log('Its time to checkout!')
+    submitModal.style.display = 'block'
+}
+
